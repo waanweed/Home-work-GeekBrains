fb55874d9df7d3c5fbf7f679c233f51c1a9fe5b7
waanweed <brainwash333@gmail.com>
2021-03-24 22:21:51 +0300

    realese v1

---
+++ Homework.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+var tempC = +prompt("Введите температуру в градусах цельсия:");
+var tempF = (9 / 5) * tempC + 32;
+alert("По фаренгейту это:" + tempF.toFixed(0));
+
+var admin = name = prompt("Введите имя:");
+alert(admin);
+
